29f3ae9297e9bfd48dd04e32c0eaf0c1e7d2176a
evariste <>
2024-10-28 21:32:14 +0000

    Example: two origin rotations in 3D.

---
+++ examples/two_origin_rots_3D.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+from objects import Glyph3D
+from transform import OriginRotation3D, Translation3D
+
+os.makedirs('output/ex3', exist_ok=True)
+
+g = Glyph3D()
+
+t = Translation3D([6, 0, 0])
+
+g = g.apply_transformation(t)
+
+R_A = OriginRotation3D([0, 1, 0], -1.0 * np.pi / 4.0)
+R_B = OriginRotation3D([0, 0, 1], np.pi / 3.0)
+
+g_A = g.apply_transformation(R_A)
+g_B = g_A.apply_transformation(R_B)
+
+g.save(f'output/ex3/glyph0.vtk')
+g_A.save(f'output/ex3/glyph1.vtk')
+g_B.save(f'output/ex3/glyph2.vtk')
