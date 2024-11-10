932bb1bbb30f363425a166295455ccd95a46abcc
evariste <>
2024-11-10 00:36:36 +0000

    Distinguish tests for trans+origin rotation and trans+general rotation.

---
+++ tests/trans_origin_rot_3D.py
@@ -0,0 +1,25 @@
+import numpy as np
+from twor.utils.general import random_rotation_3D, ensure_vec
+from twor.geom.transform import Rotation3D, TransOriginRotation3D
+
+ax, theta = random_rotation_3D()
+
+P = np.random.rand(3) * 10
+P = ensure_vec(P)
+
+rot_A = Rotation3D(P, ax, theta)
+
+M_A = rot_A.homogeneous_matrix()
+
+transf_B = rot_A.to_trans_origin_rot()
+
+assert isinstance(transf_B, TransOriginRotation3D)
+
+M_B = transf_B.homogeneous_matrix()
+
+assert np.allclose(M_A, M_B)
+
+print(rot_A)
+
+
+print(transf_B)
--- tests/trans_rot_3D.py
+++ tests/trans_rot_3D.py
@@ -1,6 +1,6 @@
 import numpy as np
 from twor.utils.general import random_rotation_3D, ensure_vec
-from twor.geom.transform import Rotation3D, TransOriginRotation3D
+from twor.geom.transform import Rotation3D, TransOriginRotation3D, TransRotation3D
 
 ax, theta = random_rotation_3D()
 
@@ -11,12 +11,40 @@ rot_A = Rotation3D(P, ax, theta)
 
 M_A = rot_A.homogeneous_matrix()
 
-
-transf_B = rot_A.to_transrot()
+transf_B = rot_A.to_trans_origin_rot()
 
 assert isinstance(transf_B, TransOriginRotation3D)
 
+M_B = transf_B.homogeneous_matrix()
+
+assert np.allclose(M_A, M_B)
+
 print(rot_A)
 
-
 print(transf_B)
+
+
+transf_C = transf_B.to_trans_rot()
+
+
+assert transf_C.is_close(transf_C)
+
+
+v, alpha = random_rotation_3D()
+Q = np.random.rand(3) * 10
+Q = ensure_vec(Q)
+
+rot_D = Rotation3D(Q, v, alpha)
+
+
+
+transf_E = rot_A.followed_by(rot_D)
+
+assert isinstance(transf_E, TransRotation3D)
+
+ax = transf_E.gen_rot.orig_rot.axis
+
+print(transf_E)
+
+
+
