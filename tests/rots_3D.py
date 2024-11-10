da2c76a41c52c48cc8acc3ebaaad6b9559a2477f
evariste <>
2024-11-10 18:36:12 +0000

    Extend tests for general 3D rotations.

--- tests/rots_3D.py
+++ tests/rots_3D.py
@@ -3,7 +3,6 @@ from twor.utils.general import random_rotation_3D, ensure_vec
 from twor.geom.transform import Rotation3D, OriginRotation3D, Translation3D
 
 ax, theta = random_rotation_3D()
-
 P = np.random.rand(3) * 10
 P = ensure_vec(P)
 
@@ -25,3 +24,60 @@ M_B = rot_B.homogeneous_matrix()
 M_C = M_trans @ M_B
 
 assert np.allclose(M_A, M_C)
+
+# Two rotations about different points with parallel axes.
+
+ax, theta = random_rotation_3D()
+P = np.random.rand(3) * 10
+P = ensure_vec(P)
+
+rot_C = Rotation3D(P, ax, theta)
+
+alpha = np.random.rand() * 2.0 * np.pi
+Q = np.random.rand(3) * 10
+Q = ensure_vec(Q)
+
+
+rot_D = Rotation3D(Q, ax, alpha)
+
+transf_E = rot_C.followed_by(rot_D)
+
+M_C = rot_C.homogeneous_matrix()
+M_D = rot_D.homogeneous_matrix()
+
+M_E = transf_E.homogeneous_matrix()
+
+success = np.allclose(M_D @ M_C, M_E)
+assert success
+
+
+# Two rotations about different points with arbitrary axes.
+
+ax, theta = random_rotation_3D()
+P = np.random.rand(3) * 10
+P = ensure_vec(P)
+
+rot_C = Rotation3D(P, ax, theta)
+
+ax2, alpha = random_rotation_3D()
+Q = np.random.rand(3) * 10
+Q = ensure_vec(Q)
+
+
+rot_D = Rotation3D(Q, ax2, alpha)
+
+transf_E = rot_C.followed_by(rot_D)
+
+M_C = rot_C.homogeneous_matrix()
+M_D = rot_D.homogeneous_matrix()
+
+M_E = transf_E.homogeneous_matrix()
+
+success = np.allclose(M_D @ M_C, M_E)
+assert success
+
+
+
+
+
+print('x')
