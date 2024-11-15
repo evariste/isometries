02f0bb16883af87ac63c8ce3907b652c2ab0bf03
evariste <>
2024-11-15 20:15:43 +0000

    Further testing for origin rotations (with quaternions).

--- tests/origin_rots_3D.py
+++ tests/origin_rots_3D.py
@@ -24,3 +24,25 @@ M_B2 = rot_B.homogeneous_matrix_B()
 
 assert np.allclose(M_A, M_A2)
 assert np.allclose(M_B, M_B2)
+
+
+
+q_A = rot_A.to_quaternion()
+q_B = rot_B.to_quaternion()
+
+q_C = rot_C.to_quaternion()
+
+v = q_C.imag
+s = np.sqrt(np.sum(v * v))
+
+c = q_C.real
+
+q_theta = np.arctan2(s, c)
+
+q_B_q_A = q_B * q_A
+
+success = np.isclose(q_B_q_A, q_C) or np.isclose(-1.0 * q_B_q_A, q_C)
+
+assert success
+
+print('Done.')
