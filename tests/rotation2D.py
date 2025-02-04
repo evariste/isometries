45133de42edef7981179ffaff3506c926cf18d98
evariste <>
2025-02-04 19:56:46 +0000

    Extend rotations in 2D test.

--- tests/rotation2D.py
+++ tests/rotation2D.py
@@ -1,8 +1,8 @@
 
 import numpy as np
-from twor.geom.transform import Rotation2D
+from twor.geom.transform import Rotation2D, OrthoRotation2D
 from twor.geom.objects import Glyph2D
-from twor.utils.general import apply_hom_matrix_to_points
+from twor.utils.general import apply_hom_matrix_to_points, apply_transform_sequence_to_glyph
 
 alpha = np.random.rand() * 2.0 * np.pi
 
@@ -18,7 +18,19 @@ glyph_rot = glyph.apply_transformation(rot)
 M = rot.get_matrix()
 pts_v2 = apply_hom_matrix_to_points(M, glyph.points)
 
-assert np.allclose(pts_v2, glyph_rot.points)
+assert np.allclose(pts_v2, glyph_rot.points), 'Result from homogeneous matrix and direct transformation do not match.'
+
+
+N = OrthoRotation2D(alpha)
+
+reflections = N.get_reflections()
+
+glyph_rot_N = glyph.apply_transformation(N)
+
+glyph_2 = apply_transform_sequence_to_glyph(reflections, glyph)
+
+
+assert np.allclose(glyph_rot_N.points, glyph_2.points), 'Results from reflections and direct orthogonal transformation do not match.'
 
 
 print('Done.')
