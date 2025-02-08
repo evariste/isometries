ee0b6beffc029c8d9eeeac56c686bf7604e08b66
evariste <>
2025-02-08 12:22:47 +0000

    Clean up test.

--- tests/reflection2D.py
+++ tests/reflection2D.py
@@ -1,64 +1,116 @@
+import sys
 import numpy as np
 from matplotlib import pyplot as plt
+
+
 from twor.geom.objects import Line2D, Glyph2D
 from twor.geom.transform_2d import Reflection2D
 from twor.utils.general import apply_hom_matrix_to_points
 from twor.utils.plotting import set_axis_glyph_bounds
 
-# Random reflection.
-P = np.random.rand(2) * 10
-v = np.random.rand(2)
-line_1 = Line2D(P, v)
-refl_1 = Reflection2D(line_1)
+def main():
 
-# Apply to a glyph.
-glyph = Glyph2D()
-glyph_refl =  glyph.apply_transformation(refl_1)
+    test_apply_reflection()
 
-# Apply via the matrix.
-M = refl_1.get_matrix()
-pts_v2 = apply_hom_matrix_to_points(M,  glyph.points)
+    test_two_step_form()
 
-assert np.allclose(pts_v2, glyph_refl.points), 'Homogeneous matrix gives different answer.'
+    test_composition()
+
+    visualise_reflection()
+
+    return 0
 
 
-# Get the two step form
-[N, t] = refl_1.two_step_form()
 
-pts_v3 = t.apply(N.apply(glyph.points))
-assert np.allclose(pts_v2, glyph_refl.points), 'Two-step form gives different answer.'
+def test_apply_reflection():
+    refl = make_random_reflection()
 
-# Another reflection
-Q = np.random.rand(2) * 10
-u = np.random.rand(2)
-line_2 = Line2D(Q, u)
-refl_2 = Reflection2D(line_2)
+    # Apply to a glyph.
+    glyph = Glyph2D()
+    glyph_refl =  glyph.apply_transformation(refl)
 
-# Composition
-refl_21 = refl_1.followed_by(refl_2)
-glyph_comp = glyph.apply_transformation(refl_21)
+    # Apply via the matrix.
+    M = refl.get_matrix()
+    pts_v2 = apply_hom_matrix_to_points(M,  glyph.points)
 
-S = refl_2.get_matrix()
+    assert np.allclose(pts_v2, glyph_refl.points), 'Homogeneous matrix gives different answer.'
 
-pts_v4 = apply_hom_matrix_to_points(S @ M, glyph.points)
-assert np.allclose(pts_v4, glyph_comp.points), 'Composition result different from one obtained with hom. matrices.'
+    return
+
+def make_random_reflection():
+    # Random reflection.
+    P = np.random.rand(2) * 10
+    v = np.random.rand(2)
+    line_1 = Line2D(P, v)
+    refl = Reflection2D(line_1)
+
+    return refl
 
 
-fig, ax = plt.subplots()
+def test_two_step_form():
+    refl = make_random_reflection()
+    glyph = Glyph2D()
 
-patch1 = glyph.get_patch(facecolor='g')
-patch2 = glyph_refl.get_patch(facecolor='r', alpha=0.5)
+    # Get the two step form
+    [N, t] = refl.two_step_form()
 
-for p in [patch1, patch2]:
-    ax.add_patch(p)
+    glyph_refl = glyph.apply_transformation(refl)
 
-xy0, xy1 = set_axis_glyph_bounds(ax, [glyph, glyph_refl])
+    glyph_B = glyph.apply_transformation(N).apply_transformation(t)
 
-f_x0 = line_1.f_x(xy0)
-f_x1 = line_1.f_x(xy1)
+    assert np.allclose(glyph_B.points, glyph_refl.points), 'Two-step form gives different answer.'
 
-ax.plot([xy0, xy1], [f_x0, f_x1], 'k:', linewidth=1)
+    return
 
-ax.set_aspect('equal', 'box')
+def test_composition():
+    refl_1 = make_random_reflection()
+    refl_2 = make_random_reflection()
+    glyph = Glyph2D()
 
-plt.show()
+    # Composition
+    transf_comp = refl_1.followed_by(refl_2)
+    glyph_comp = glyph.apply_transformation(transf_comp)
+
+    M = refl_1.get_matrix()
+    N = refl_2.get_matrix()
+
+    NM = N @ M
+
+    pts_via_matrices = apply_hom_matrix_to_points(NM, glyph.points)
+
+    assert np.allclose(pts_via_matrices, glyph_comp.points), 'Composition result different from one obtained with hom. matrices.'
+
+    return
+
+def visualise_reflection():
+    refl = make_random_reflection()
+    line = refl.line
+    glyph = Glyph2D()
+
+
+    glyph_refl = glyph.apply_transformation(refl)
+
+    fig, ax = plt.subplots()
+
+    patch1 = glyph.get_patch(facecolor='g')
+    patch2 = glyph_refl.get_patch(facecolor='r', alpha=0.5)
+
+    for p in [patch1, patch2]:
+        ax.add_patch(p)
+
+    xy0, xy1 = set_axis_glyph_bounds(ax, [glyph, glyph_refl])
+
+    f_x0 = line.f_x(xy0)
+    f_x1 = line.f_x(xy1)
+
+    ax.plot([xy0, xy1], [f_x0, f_x1], 'k:', linewidth=1)
+
+    ax.set_aspect('equal', 'box')
+
+    plt.show()
+
+    return
+
+
+if __name__ == '__main__':
+    sys.exit(main())
