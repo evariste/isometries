83bda911fffbb4a0942bffad8866289110df15de
evariste <>
2025-02-09 14:08:14 +0000

    New reflection test.

--- tests/reflection_3D.py
+++ tests/reflection_3D.py
@@ -2,6 +2,7 @@ import sys
 
 import numpy as np
 
+from tests.rot_to_reflections import ref_L
 from twor.utils.general import apply_hom_matrix_to_points
 from twor.geom.objects import Glyph3D
 from twor.geom.transform_3d import random_reflection_3d, random_ortho_reflection_3d
@@ -11,8 +12,26 @@ def main():
 
     test_matrices()
 
+    test_self_inversion()
+
     return 0
 
+
+def test_self_inversion():
+
+    ortho_refl = random_ortho_reflection_3d()
+
+    glyph = Glyph3D()
+
+    glyph_B = glyph.apply_transformation(ortho_refl).apply_transformation(ortho_refl)
+    assert glyph.is_close_to(glyph_B), 'Failed self inversion check'
+
+    refl = random_reflection_3d()
+
+    glyph_C = glyph.apply_transformation(refl).apply_transformation(refl)
+    assert glyph.is_close_to(glyph_C), 'Failed self inversion check'
+
+    return
 
 def test_matrices():
 
