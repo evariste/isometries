856f96cb67b899d9d761aed541955cff1efa5fe7
evariste <>
2025-02-09 13:13:05 +0000

    Moved out visualisation part.

--- tests/reflection2D.py
+++ tests/reflection2D.py
@@ -16,8 +16,6 @@ def main():
 
     test_composition()
 
-    visualise_reflection()
-
     return 0
 
 
@@ -74,34 +72,6 @@ def test_composition():
 
     return
 
-def visualise_reflection():
-    refl = random_reflection2d()
-    line = refl.line
-    glyph = Glyph2D()
-
-
-    glyph_refl = glyph.apply_transformation(refl)
-
-    fig, ax = plt.subplots()
-
-    patch1 = glyph.get_patch(facecolor='g')
-    patch2 = glyph_refl.get_patch(facecolor='r', alpha=0.5)
-
-    for p in [patch1, patch2]:
-        ax.add_patch(p)
-
-    xy0, xy1 = set_axis_glyph_bounds(ax, [glyph, glyph_refl])
-
-    f_x0 = line.f_x(xy0)
-    f_x1 = line.f_x(xy1)
-
-    ax.plot([xy0, xy1], [f_x0, f_x1], 'k:', linewidth=1)
-
-    ax.set_aspect('equal', 'box')
-
-    plt.show()
-
-    return
 
 
 if __name__ == '__main__':
